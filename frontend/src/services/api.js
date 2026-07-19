fcf38c3b2d74ac79eb73d272f5c1f609a22bfbec
DadiSoumia <163993573+DadiSoumia@users.noreply.github.com>
2026-07-19 02:20:51 +0200

    updat

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://192.168.100.4:5000/api";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
